31a078229727a4be1b7976141b807497398abefd
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-09-24 19:34:26 -0500

    Slightly modified version after completing it live in class.

--- heap_sort.c
+++ heap_sort.c
@@ -1,33 +1,39 @@
 #include<stdio.h>
 
+void heapify(int *, int);
+void printHeapElements(int *);
+int extractMin(int *);
+void sinkDown(int *, int);
+
 int values[50] = {8, 3, 5, 10, 38, 4, 27, 2, 9, 34};
 int heapIndex = 9;
-
-// 
-
-// 1, 5, 6, 9, 10, 13, 12
-//      1
-//  5       6
-// 9  10  13  12
-
-void heapify(int *, int);
+const int heapSize = 9;
 
 int main() {
+  printHeapElements(values);
 
   for (int i=heapIndex; i>=0; i--) {
     heapify(values, i);
   }
+
+  int sorted[9];
+  for (int i= 0; i<=heapSize; i++) {
+    int min = extractMin(values);
+    sorted[i] = min;
+  }
+
+  printHeapElements(sorted);
 }
 
 void heapify(int * values, int currentIndex) {
-  int left = 2 * currentIndex + 1;
-  int right = 2 * currentIndex + 2;
-
   int min = currentIndex;
-  if (left <= currentIndex && values[left] < values[min]) {
+  int left = 2*currentIndex + 1;
+  int right = 2*currentIndex + 2;
+
+  if (left <= heapIndex && values[left] < values[min]) {
     min = left;
   }
-  if (right <= currentIndex && values[right] < values[min]) {
+  if (right <= heapIndex && values[right] < values[min]) {
     min = right;
   }
 
@@ -38,4 +44,39 @@ void heapify(int * values, int currentIndex) {
 
     heapify(values, min);
   }
+}
+
+int extractMin(int * heap) {
+  int min = heap[0];
+  heap[0] = heap[heapIndex--];
+  sinkDown(heap, 0);
+  return min;
+}
+
+void sinkDown(int * heap, int currentIndex) {
+  int left = 2 * currentIndex + 1;
+  int right = 2 * currentIndex + 2;
+
+  int min = currentIndex;
+  if (left <= heapIndex && heap[left] < heap[min]) {
+    min = left;
+  }
+  if (right <= heapIndex && heap[right] < heap[min]) {
+    min = right;
+  }
+
+  int temp = heap[min];
+  heap[min] = heap[currentIndex];
+  heap[currentIndex] = temp;
+
+  if (min != currentIndex) {
+    sinkDown(heap, min);
+  }
+}
+
+void printHeapElements(int * values) {
+  for (int i = 0; i<=heapSize; i++) {
+    printf("%d ", values[i]);
+  }
+  printf("\n");
 }
